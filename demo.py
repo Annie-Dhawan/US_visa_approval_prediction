d182e12062d1fb01ef0884707fc3343ea05e9a3e
Annie Dhawan <anniedhawana1999@gmail.com>
2024-03-03 22:47:46 +0530

    exxceptions and logger added

---
+++ demo.py
@@ -0,0 +1,11 @@
+import sys
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+
+logging.info("Welcome to our Custom log")
+
+try:
+    a = 1/"10"
+except Exception as e:
+    raise USvisaException(e,sys) from e
